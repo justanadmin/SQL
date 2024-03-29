9b9a457265722b93f319f7c9dc71c052e76e0091
justanadmin <103416575+justanadmin@users.noreply.github.com>
2024-03-30 01:37:13 +0530

    Update Basics_Day0.sql

--- Basics_Day0.sql
+++ Basics_Day0.sql
@@ -6,8 +6,12 @@ SQL INSERT into TABLE
 SQL DROP table 
 */
 
-show databases --> Check the databases 
-
-show databases;
-use world
+show databases /* Check the databases */ 
+SELECT * FROM "BOOKLIST" LIMIT 10 ;/*We have this option in SQL server GUI too we can simply get the top 10 entries via command too */
+/*Introduction to WHERE*/
+SELECT "title","author" FROM "BOOKLIST" where "year"=2023;
+SELECT "title","author" FROM "BOOKLIST" where "year"!=2023;
+SELECT "title","author" FROM "BOOKLIST" where "year"<>2023;
+/*Recom : We can use "" for the SQL identifiers and '' for all the strings that are listed 
+We have = which is kind of self explainatory !=  not equal <> other than */
 
