02f6a9e7381c312a6347cf950af4505cdc9ba280
justanadmin <103416575+justanadmin@users.noreply.github.com>
2024-03-29 19:58:19 +0530

    Update and rename Basics_Day0 to Basics_Day0.sql

---
+++ Basics_Day0.sql
@@ -0,0 +1,13 @@
+/*
+SQL CREATE databases
+SQL DROP databases
+SQL CREATE Table
+SQL INSERT into TABLE 
+SQL DROP table 
+*/
+
+show databases --> Check the databases 
+
+show databases;
+use world
+
