05c14f0a4727de763755c948449d0be0ae97d2be
justanadmin <103416575+justanadmin@users.noreply.github.com>
2024-03-30 01:49:01 +0530

    Update Basics_Day0.sql

--- Basics_Day0.sql
+++ Basics_Day0.sql
@@ -15,8 +15,11 @@ SELECT "title","author" FROM "BOOKLIST" where "year"<>2023;
 /*Recom : We can use "" for the SQL identifiers and '' for all the strings that are listed 
 We have = which is kind of self explainatory !=  not equal <> other than */
 SELECT "title","author" FROM "BOOKLIST" WHERE NOT "year"=2023;
-/* Merging condition */
+/* Merging condition : Compound Conditions*/
 SELECT "title","format" FROM "BOOKLIST" where "format" <> 'hardcover' and "year" <> 2023;
 /*AND OR helps to make more complex conditions , we will also understand () use of brackets*/
 SELECT "title","format" FROM "BOOKLIST" where "format" <> 'hardcover' and "year" <> 2023;
 SELECT "title","format" FROM "BOOKLIST" where ("year" = 2023 and "year" = 2022) and "format" <> 'hardcover';
+/*IS NULL and IS NOT NULL */
+SELECT "title","translator" FROM "BOOKLIST" where "translator" IS NULL;
+SELECT "title","translator" FROM "BOOKLIST" where "translator" IS NOT NULL;
